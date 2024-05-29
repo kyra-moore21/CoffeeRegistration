567a76552b34f8c052a8df1e566e532112371947
Kyra Moore <kyra.moore21@yahoo.com>
2024-05-29 14:22:55 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Coffee_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Result(UserInfo u)
+        {
+            return View(u);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
